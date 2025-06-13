abb053e4b6f86572118994a0eb4eec6630f9c812
Ryu Donghyeon <r12dh16@gmail.com>
2025-06-13 16:59:58 +0900

    feat: s3 버킷에 배포

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  // 이미지 설정 (Next.js 이미지 최적화가 정적 내보내기에서는 작동하지 않음)
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   async rewrites() {
     return [
